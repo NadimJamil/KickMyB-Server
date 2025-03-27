25050f2217aa69746b0b94bbd9fdb65384c2b5cc
Nadim Jamil <2261561@cegepmontpetit.ca>
2025-03-27 14:32:25 -0400

    FCT Method cancel()

--- src/main/java/org/kickmyb/server/account/ControllerAccount.java
+++ src/main/java/org/kickmyb/server/account/ControllerAccount.java
@@ -8,6 +8,8 @@ import org.kickmyb.transfer.SigninRequest;
 import org.kickmyb.transfer.SigninResponse;
 import org.kickmyb.transfer.SignupRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,9 +17,11 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.UnknownHttpStatusCodeException;
 
 
 // Modified for Spring Boot 3 https://docs.spring.io/spring-security/reference/servlet/authentication/persistence.html
@@ -85,5 +89,11 @@ public class ControllerAccount {
         SecurityContextHolder.setContext(context);
         securityContextRepository.saveContext(context, request, response);
         return "";
+    }
+
+
+    @GetMapping("/api/cancel")
+    public ResponseEntity<java.lang.String> cancel(){
+        return ResponseEntity.status(498).body("yo");
     }
 }
